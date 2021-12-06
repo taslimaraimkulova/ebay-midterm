677aef54f7bf35aa3547865f27e4c1b322d82115
Danial <80159164+TurboDanik@users.noreply.github.com>
2021-12-06 11:03:35 +0600

    Update registration.js

--- midterm2/registration.js
+++ midterm2/registration.js
@@ -37,7 +37,7 @@ window.addEventListener("load", function(event) {
 				flag = true;
 			}
 		}
-		if (!flag) {
+		if (flag) {
 			localStorage.setItem(findMaxIndex() + 1, JSON.stringify(admin));
 		}
 	}
